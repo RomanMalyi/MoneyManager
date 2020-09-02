1f26401449d3b2166b3474809b3a0c0644ab0903
Roman Malyi <madphysicsm@gmail.com>
2020-09-02 22:45:28 +0300

    added empty solution

---
+++ src/MoneyManager/MoneyManager/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyManager.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get([FromQuery] int skip = 0, [FromQuery] int take = 10)
+        {
+            return Ok();
+        }
+    }
+}
